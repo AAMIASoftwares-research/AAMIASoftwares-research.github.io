fc36d9ab3f88bd76c28499f7beafb86e8226e220
Matteoleccardi <leccardi.matteo@gmail.com>
2025-06-26 14:20:52 +0200

    Basic logic works, data saves, only images missing

--- EMBC2025/js/app.js
+++ EMBC2025/js/app.js
@@ -1,11 +1,34 @@
 
+async function saveData(save_data) {
+    const password = 'sw4567890plkj';
+    const server_url = 'http://shapire.altervista.org/EMBC2025/save_data.php';
+    // This code is on my shapire server
+
+    try {
+        const response = await fetch(server_url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                data: save_data,
+                password: password
+            })
+        });
+
+        const result = await response.json(); // Assuming PHP returns JSON
+
+        if (! response.ok)
+            console.error('Error saving data');
+
+        console.log(result);
+
+    } catch (error) {
+        console.error('Network error or problem with request:', error);
+    }
 
 
-
-
-
-
-
+}
 
 
 
@@ -26,31 +49,46 @@ function appSetup() {
     const action_button = document.querySelector('button.action');
 
 
-    picture_div.addEventListener('click', function (event) {
-        bullet.style.display = 'block';
-        bullet.style.top = event.pageY + 'px';
-        bullet.style.left = event.pageX + 'px';
-    });
-
+    /// START PAGE ///
+    
     start_button.addEventListener('click', function (event) {
         // hide button and instructions
-        start_button.style.display = 'none';
-        instructions.style.display = 'none';
-
+        start_button.classList.add('hidden');
+        instructions.classList.add('hidden');
+        
         // show the other elements
-        title.style.display = '';
-        picture_div.style.display = '';
-        bullet.style.display = 'none'; // keep bullet hidden until clicked
-        action_button.style.display = '';
+        title.classList.remove('hidden');
+        picture_div.classList.remove('hidden');
+        bullet.classList.add('hidden');
+        action_button.classList.remove('hidden');
 
+        // set save button to inactive
+        action_button.disabled = true;
+
+        // advance state
         state = 'right';
     });
 
+    /// BULLET SELECTION and SAVE ///
+    
+    
+    picture_div.addEventListener('click', function (event) {
+        bullet.classList.remove('hidden');
+        bullet.style.top = event.pageY + 'px';
+        bullet.style.left = event.pageX + 'px';
 
+        // set save button to active since  apoint was placed
+        action_button.disabled = false;
+    });
+
+    var save_data = {
+        'image_id': '',
+        'right (percentLeft, percentTop)': [],
+        'left (percentLeft, percentTop)': [],
+    }
 
     action_button.addEventListener('click', function (event) {
-        if (bullet.style.display == 'none')
-            return
+        
         const left = parseInt(bullet.style.left, 10);
         const top = parseInt(bullet.style.top, 10);
 
@@ -63,23 +101,59 @@ function appSetup() {
         const relX = left - divLeft;
         const relY = top - divTop;
 
-        const percentX = ((relX / divWidth) * 100).toFixed(3);
-        const percentY = ((relY / divHeight) * 100).toFixed(3);
+        const percentLeft = ((relX / divWidth) * 100).toFixed(3);
+        const percentTop = ((relY / divHeight) * 100).toFixed(3);
 
         switch (state) {
             case 'right':
+                // save right ostium position
+                save_data['right (percentLeft, percentTop)'] = [percentLeft, percentTop]
+                // Change title
+                title.innerHTML = 'Please select the left coronary ostium';
+                // load next image
+
+                // hide bullet and change color to blue
+                bullet.classList.add('hidden');
+                bullet.classList.add('left');
+
+                // change save button to inactive and change text
+                action_button.disabled = true;
+                action_button.innerHTML = 'Save Left Ostium Position';
+                // advance with state
                 state = 'left';
                 break;
 
             case 'left':
-                state = 'finish';
+                // save left ostium position
+                save_data['left (percentLeft, percentTop)'] = [percentLeft, percentTop]
+                // hide image div
+                picture_div.classList.add('hidden');
+                // hide bullet
+                bullet.classList.add('hidden');
+                // change title
+                title.innerHTML = 'Click the button below to submit your answer';
+                // change action button
+                action_button.innerHTML = 'SUBMIT';
+                // change state
+                state = 'save';
                 break;
 
-            case 'finish':
+            case 'save':
+                // store answer
+                saveData(save_data);
+
+                // reload page after 5 seconds
+                title.innerHTML = 'Thank you!';
+                action_button.classList.add('hidden');
+                window.setTimeout(
+                    function () {
+                        window.location.replace('./');
+                    },
+                    5000
+                )
                 break;
         }
 
-        console.log(`Saved click at: ${percentX}% X, ${percentY}% Y`);
     });
 
 }
